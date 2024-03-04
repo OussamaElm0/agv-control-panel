30b4b29cdece8a78a980f89dbf7c33d96338c3a4
Oussama El Mabrouki <138411649+OussamaElm0@users.noreply.github.com>
2024-03-04 11:11:04 +0100

    Create agv's model

---
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,9 @@
+
+=
+agv/models.py,4\c\4cd9b68acdaa276cc4555bd939b0527397138665
+<
+agv/admin.py,3\7\372c4b3561861f4d4b151566e82c91625b1b1144
+D
+Sinoftec/settings.py,1\3\13bd0f56f6281755c75be8bd1e1ca0f7e4e26714
+;
+agv/apps.py,d\7\d7f31daf73a8d1eed9bb5b0d8f022c0678f755c0
--- agv/models.py
+++ agv/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Agv(models.Model):
+    statusOptions = (
+        ('Inactive', 'Inactive'),
+        ('Porter une charge', 'Porter une charge'),
+        ('Mise en charge', 'Mise en charge')
+    )
+
+    nom = models.CharField(max_length=100)
+    capacite = models.IntegerField()
+    vie_batterie = models.IntegerField()
+    status = models.CharField(max_length=50,choices=statusOptions)
+
+    def __str__(self):
+        return self.nomAgv
