a9bbc4f71281da5f98f8cc3608b88bb445766424
Oussama El Mabrouki <138411649+OussamaElm0@users.noreply.github.com>
2024-03-06 14:53:03 +0100

    Create command's model

--- commandes/admin.py
+++ commandes/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+@admin.register(Commande)
+class CommandeAdmin(admin.ModelAdmin):
+    list_display= (
+        'id',
+        'id_agv',
+        'id_bloc',
+        'date',
+        'time',
+        'confirmed'
+    )
--- commandes/models.py
+++ commandes/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from agv.models import Agv
+from blocs.models import Bloc
+import datetime as dt
+from django.utils import timezone
+
 
 # Create your models here.
+class Commande(models.Model):
+    id_agv = models.ForeignKey(Agv, on_delete=models.CASCADE)
+    id_bloc = models.ForeignKey(Bloc, on_delete=models.CASCADE)
+    date = dt.datetime.now().strftime("%Y-%m-%d")
+    time = dt.datetime.now().strftime("%H:%M:%S")
+    confirmed = models.BooleanField(default=False)
+
+    def __str__(self) -> str:
+        return f'Agv: {self.id_agv} to Bloc: {self.id_bloc}'
