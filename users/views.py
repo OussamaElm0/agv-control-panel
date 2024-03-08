49074ab866e1b35d290347d827af5899e23c9e58
Oussama El Mabrouki <138411649+OussamaElm0@users.noreply.github.com>
2024-03-08 12:47:59 +0100

    Add exception if admin not found

--- users/views.py
+++ users/views.py
@@ -15,17 +15,24 @@ def adminLogin(request):
         if form.is_valid():
             username = form.cleaned_data['username']
             password = form.cleaned_data['password']
-            admin = authenticate(request,username=username, password=password)
-            print(admin)
-            if admin is not None:
-                # User authentication successful
-                login(request, admin)
-                return HttpResponseRedirect(test)
-            else:
-                # User authentication failed
+            try :
+                user = User.objects.get(username=username, password=password)
+                print(user)
+                admin = authenticate(request,username=username, password=password)
+                print(admin)
+                if admin is not None:
+                    # User authentication successful
+                    login(request, admin)
+                    return render(request, 'users/admin/home.html')
+                else:
+                    # User authentication failed
+                    error_message = 'Invalid username or password.'
+                    return render(request, 'users/admin/login.html', {'form': form, 'error_message': error_message})
+            except User.DoesNotExist:
                 error_message = 'Invalid username or password.'
                 return render(request, 'users/admin/login.html', {'form': form, 'error_message': error_message})
         else:
+            print(form.errors)
             return render(request, 'users/admin/login.html', {'form': form})
 
 def test(request):
