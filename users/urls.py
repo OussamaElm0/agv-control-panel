d688a40d1f0aa78d6620b756ca2ae28ca74c43b0
Oussama El Mabrouki <138411649+OussamaElm0@users.noreply.github.com>
2024-03-09 18:56:32 +0100

    Handle admin login form in one view

--- users/urls.py
+++ users/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('admin/login', loginForm, name='loginForm'),
-    path('admin/checkIfAdmin', checkIfAdmin, name='checkIfAdmin'),
+    path('admin/login', checkIfAdmin, name='loginForm'),
+    # path('admin/checkIfAdmin', checkIfAdmin, name='checkIfAdmin'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,28 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import *
 from .forms import *
 from django.contrib.auth import authenticate, login, logout
-from django.contrib import messages
-from django.http import HttpResponseRedirect, HttpResponse
 from django.views.decorators.http import *
 
-# Create your views here.
-@require_GET
-def loginForm(request):
-    if request.method == 'GET':
-        form = AdminLoginForm()
-        return render(request, 'users/admin/login.html', {'form': form})
-
-@require_POST
 def checkIfAdmin(request):
-    # if request.method == 'POST':
-    username = request.POST.get('username')
-    password = request.POST.get('password')
-    user = authenticate(request, username=username, password=password)
-    if user is not None:
-        login(request, user)
-        return HttpResponse('logged_in_message')
-    else:
-            # Handle invalid login credentials
-        error_message = "Invalid username or password."
-        return HttpResponseRedirect('login', {'error_message': error_message})
+    if request.method == 'POST':
+        form = AdminLoginForm(request.POST)
+        if form.is_valid: 
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+            user = authenticate(request,username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('loginForm')
+            else :
+                return render(request,'users/admin/login.html', {
+                    'form': form,
+                    'error_message': 'Username or password invalid'
+                })
+        return  render(request,'users/admin/login.html', {
+                    'form': form,
+                    'error_message': 'Form fields not valid'
+                })
+    form = AdminLoginForm()
+    return render(request,'users/admin/login.html', {
+        'form': form
+    })
