53278c9cf727ccbf5aad8abf1eecfcb07ba7ebb1
Oussama El Mabrouki <138411649+OussamaElm0@users.noreply.github.com>
2024-03-09 13:06:41 +0100

    Authenticate the user

--- users/urls.py
+++ users/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('admin/login', adminLogin, name='adminLogin'),
-    path('admin/home', test, name='test'),
+    path('admin/login', loginForm, name='loginForm'),
+    path('admin/checkIfAdmin', checkIfAdmin, name='checkIfAdmin'),
 ]
--- users/views.py
+++ users/views.py
@@ -3,37 +3,26 @@ from .models import *
 from .forms import *
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponse
+from django.views.decorators.http import *
 
 # Create your views here.
-def adminLogin(request):
-   if request.method == 'GET':
+@require_GET
+def loginForm(request):
+    if request.method == 'GET':
         form = AdminLoginForm()
         return render(request, 'users/admin/login.html', {'form': form})
-   else :
-        form = AdminLoginForm(request.POST)
-        if form.is_valid():
-            username = form.cleaned_data['username']
-            password = form.cleaned_data['password']
-            try :
-                user = User.objects.get(username=username, password=password)
-                print(user)
-                admin = authenticate(request,username=username, password=password)
-                print(admin)
-                if admin is not None:
-                    # User authentication successful
-                    login(request, admin)
-                    return render(request, 'users/admin/home.html')
-                else:
-                    # User authentication failed
-                    error_message = 'Invalid username or password.'
-                    return render(request, 'users/admin/login.html', {'form': form, 'error_message': error_message})
-            except User.DoesNotExist:
-                error_message = 'Invalid username or password.'
-                return render(request, 'users/admin/login.html', {'form': form, 'error_message': error_message})
-        else:
-            print(form.errors)
-            return render(request, 'users/admin/login.html', {'form': form})
 
-def test(request):
-    return render(request, 'users/admin/home.html')
+@require_POST
+def checkIfAdmin(request):
+    # if request.method == 'POST':
+    username = request.POST.get('username')
+    password = request.POST.get('password')
+    user = authenticate(request, username=username, password=password)
+    if user is not None:
+        login(request, user)
+        return HttpResponse('logged_in_message')
+    else:
+            # Handle invalid login credentials
+        error_message = "Invalid username or password."
+        return HttpResponseRedirect('login', {'error_message': error_message})
