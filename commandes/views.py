1732e2dbb066bbcd745c6f810bc71d09b6ea8960
Oussama El Mabrouki <138411649+OussamaElm0@users.noreply.github.com>
2024-03-12 11:29:01 +0000

    Add date and time to the view whose create command

--- commandes/views.py
+++ commandes/views.py
@@ -2,10 +2,14 @@ from django.shortcuts import render
 from agv.models import Agv
 from blocs.models import Bloc
 from .models import Commande
+import datetime as dt
 
 # Create your views here.
 def sendCommand(agvId, blocID):
     agv = Agv.objects.get(pk=agvId)
     bloc = Bloc.objects.get(pk=blocID)
-    command = Commande(id_agv=agv, id_bloc= bloc)
+    date = dt.datetime.now().strftime("%Y-%m-%d")
+    time = dt.datetime.now().strftime("%H:%M")
+
+    command = Commande(id_agv=agv, id_bloc= bloc, date= date, time= time)
     command.save()
