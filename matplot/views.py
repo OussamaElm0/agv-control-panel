c389139b1bc1f9a09baf554e00155eafd368d8f1
Oussama El Mabrouki <138411649+OussamaElm0@users.noreply.github.com>
2024-03-28 12:13:22 +0000

    Add plot for commads last week

--- matplot/views.py
+++ matplot/views.py
@@ -1,9 +1,6 @@
-# Import necessary libraries
 import os
 import numpy as np
 import matplotlib.pyplot as plt
-from io import BytesIO
-import base64
 from django.conf import settings
 from django.utils import timezone
 from datetime import timedelta
@@ -11,31 +8,42 @@ from django.db.models import Count
 from commandes.models import Commande
 
 def commands_last_week():
-    # Calculate the start and end dates for the last week
-    end_date = timezone.now()
-    start_date = end_date - timedelta(days=7)
+    try:
+        # Calculate the start and end dates for the last week
+        end_date = timezone.now()
+        start_date = end_date - timedelta(days=7)
 
-    # Filter Commande objects for the last week
-    commandes_last_week = Commande.objects.filter(date__gte=start_date, date__lte=end_date)
+        # Filter Commande objects for the last week
+        commandes_last_week = Commande.objects.filter(date__gte=start_date, date__lte=end_date)
 
-    # Group Commande objects by date and count the number of Commandes for each date
-    commands_per_day_last_week = commandes_last_week.values('date').annotate(total_commandes=Count('id'))
+        # Group Commande objects by date and count the number of Commandes for each date
+        commands_per_day_last_week = commandes_last_week.values('date').annotate(total_commandes=Count('id'))
 
-    # Extract dates and corresponding command counts
-    dates = [entry['date'] for entry in commands_per_day_last_week]
-    command_counts = [entry['total_commandes'] for entry in commands_per_day_last_week]
+        # Extract dates and corresponding command counts
+        dates = [entry['date'] for entry in commands_per_day_last_week]
+        command_counts = [entry['total_commandes'] for entry in commands_per_day_last_week]
 
-    # Create the plot
-    plt.figure(figsize=(10, 6))
-    plt.plot(dates, command_counts, marker='o', linestyle='-')
-    plt.xlabel('Date')
-    plt.ylabel('Number of Commands')
-    plt.title('Commands per Day in Last Week')
-    plt.xticks(rotation=45)  # Rotate x-axis labels for better readability
-    plt.tight_layout()
+        # Create the plot
+        plt.figure(figsize=(10, 6))
+        plt.plot(dates, command_counts, marker='o', linestyle='-')
+        plt.xlabel('Date')
+        plt.ylabel('Number of Commands')
+        plt.title('Commands per Day in Last Week')
+        plt.xticks(rotation=45)  # Rotate x-axis labels for better readability
+        plt.tight_layout()
 
-    # Display the plot
-    plt.show()
+        # Define the path for saving the plot
+        plot_dir = os.path.join(settings.STATIC_URL, 'images')
+        os.makedirs(plot_dir, exist_ok=True)
+        plot_path = 'static/images/charts/commands_last_week.png'
 
-# Call the function to generate and display the plot
-commands_last_week()
+        # Save the plot to the file
+        plt.savefig(plot_path)
+
+        # Close the plot to release resources
+        plt.close('all')
+
+        return plot_path
+    except Exception as e:
+        print("Error occurred:", e)
+        return None
--- users/views.py
+++ users/views.py
@@ -12,34 +12,38 @@ from commandes.models import Commande
 from django.http import HttpResponse
 from getmac import get_mac_address as gma
 from django.core.paginator import Paginator
+from matplot.views import commands_last_week
+
+
 # Create your views here.
 
-#View function to check if the user is an admin and handle the login process.
+# View function to check if the user is an admin and handle the login process.
 def checkIfAdmin(request):
     if request.method == 'POST':
         form = AdminLoginForm(request.POST)
         if form.is_valid:
             username = request.POST.get('username')
             password = request.POST.get('password')
-            user = authenticate(request,username=username, password=password)
+            user = authenticate(request, username=username, password=password)
             if user is not None:
                 login(request, user)
                 return redirect('adminIndex')
-            else :
-                return render(request,'users/admin/login.html', {
+            else:
+                return render(request, 'users/admin/login.html', {
                     'form': form,
                     'error_message': 'Username or password invalid'
                 })
-        return  render(request,'users/admin/login.html', {
-                    'form': form,
-                    'error_message': 'Form fields not valid'
-                })
+        return render(request, 'users/admin/login.html', {
+            'form': form,
+            'error_message': 'Form fields not valid'
+        })
     elif request.user.is_authenticated:
         return redirect('adminIndex')
     form = AdminLoginForm()
-    return render(request,'admin/login.html', {
+    return render(request, 'admin/login.html', {
         'form': form
     })
+
 
 # Renders the admin home page if the user is authenticated, otherwise redirects to the login form.
 @require_GET
@@ -49,10 +53,12 @@ def dashboard(request):
             'total_agvs': Agv.objects.all().count(),
             'total_blocs': Bloc.objects.all().count(),
             'total_postes': Poste.objects.all().count(),
+            'commands_last_week': commands_last_week(),
         }
         return render(request, 'admin/dashboard.html', context)
     else:
         return redirect('loginForm')
+
 
 # Logout the admin from his account
 @login_required
@@ -60,30 +66,32 @@ def adminLogout(request):
     logout(request)
     return redirect('loginForm')
 
+
 @require_GET
 def commande(request):
     form = CommandeForm()
     isAuthenticated = request.user.is_authenticated
     macAddress = Poste.objects.filter(mac_address=gma()).first()
-    if macAddress is not None or isAuthenticated :
+    if macAddress is not None or isAuthenticated:
         agvsToBloc = Commande.objects.filter(confirmed=False, id_bloc=macAddress.bloc).order_by('-id')
-        paginator = Paginator(agvsToBloc,5)
+        paginator = Paginator(agvsToBloc, 5)
         page_number = request.GET.get("page")
         page_obj = paginator.get_page(page_number)
         context = {
             'form': form,
             'agvsToBloc': page_obj
         }
-        return render(request, 'commandes/send.html',context)
-    else :
-        return render(request,'http_errors/401.html')
+        return render(request, 'commandes/send.html', context)
+    else:
+        return render(request, 'http_errors/401.html')
+
 
 @require_POST
 def sendCommand(request):
     isAuthenticated = request.user.is_authenticated
     macAddress = Poste.objects.filter(mac_address=gma()).first()
-    if macAddress is not None or isAuthenticated :
-        try :
+    if macAddress is not None or isAuthenticated:
+        try:
             agv = request.POST.get('id_agv')
             bloc = request.POST.get('id_bloc')
             newCommand(agv, bloc)
@@ -91,14 +99,15 @@ def sendCommand(request):
         except Exception as e:
             print(e)
             return HttpResponse(e)
-    else :
+    else:
         return HttpResponse('Not allowed')
+
 
 @require_POST
 def updateCommande(request, id):
     method = request.POST.get('method')
     if method == "PUT":
         commande = Commande.objects.get(pk=id)
-        commande.confirmed =True
+        commande.confirmed = True
         commande.save()
     return redirect('/')
