7c0cc85675a90616b30ad955291d59bee6fa03ab
Oussama El Mabrouki <138411649+OussamaElm0@users.noreply.github.com>
2024-03-31 15:16:11 +0000

    Fix most agvs used chart displaying

--- static/js/dashboard/most_agvs_used.js
+++ static/js/dashboard/most_agvs_used.js
@@ -1,35 +1,49 @@
 let mostAgvUsed = window.mostAgvsUsed;
-
+console.log(mostAgvUsed)
 // Extract AGV names and usage counts from the data
 const labels = mostAgvUsed.map(({ name }) => name);
-const usageCounts = mostAgvUsed.map(({ usage_count }) => usage_count);
+const usageCounts = mostAgvUsed.map(({ usage }) => usage);
 
-const data = {
-    labels,
-    datasets: [
-        {
-        label: 'AGV Usage Count',
-        data: usageCounts,
-        backgroundColor: 'rgba(54, 162, 235, 0.2)',
-        borderColor: 'rgba(54, 162, 235, 1)',
-        borderWidth: 1,
-        },
+console.log(usageCounts)
+
+// Chart data
+const pieChartData = {
+  labels: labels,
+  datasets: [{
+    label: 'AGV Usage Count',
+    data: usageCounts,
+    backgroundColor: [
+      'rgb(255, 99, 132)',
+      'rgb(255, 159, 64)',
+      'rgb(255, 205, 86)',
+      'rgb(75, 192, 192)',
+      'rgb(54, 162, 235)'
     ],
+    borderWidth: 1
+  }]
 };
 
-const config = {
-    type: 'bar',
-    data,
-    options: {
-        scales: {
-            y: {
-                beginAtZero: true
-            }
-        }
+// Chart configuration
+const pieChartConfig = {
+  type: 'pie',
+  data: pieChartData,
+  options: {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: 'top',
+      },
+      title: {
+        display: true,
+        text: 'Most AGVs Used'
+      }
     }
+  }
 };
 
-const mostAgvsUsedChart = new Chart(
-  document.getElementById("most-agvs-used").getContext(''),
-  config
-);  
+// Render the pie chart
+const ctxPie = document.getElementById('most-agvs-used').getContext("2d");
+const agvsChart = new Chart(
+    ctxPie
+    , pieChartConfig
+)
